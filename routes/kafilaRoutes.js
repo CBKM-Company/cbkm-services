6d9c7974e6db6d1fc912993d246a81e92473f1db
Prakhar-Mangal <mangalprakhar07@gmail.com>
2019-09-01 03:12:43 +0530

    resgister1

--- routes/kafilaRoutes.js
+++ routes/kafilaRoutes.js
@@ -1,4 +1,5 @@
 const app = module.exports = require('express')();
 app.use('/', require('express').static('kafila'))
+app.use('/register', require('express').static('kafila/register.html'))
 app.use('/contact', require('./contactRoute'))
 app.use('/register', require('./registerKafilaRoute'))

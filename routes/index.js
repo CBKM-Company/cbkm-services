e2448ad407572e13413e0c9dfdcd0e7c36387cbc
cbkm-company <you@example.com>
2019-09-02 02:45:25 +0530

    regis

--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,4 @@ const app = module.exports = require('express')();
 app.use('/contact', require('./contactRoute'))
 app.use('/user', require('./userRoute'))
 app.use('/kafila', require('./kafilaRoutes'))
+app.use('/register', require('./registerKafilaRoute'))

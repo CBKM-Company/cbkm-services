1084cf38290c3ddefb48f2deec0ab3ec070ccdb9
Prakhar-Mangal <mangalprakhar07@gmail.com>
2019-09-02 02:37:49 +0530

    saho

--- routes/kafilaRoutes.js
+++ routes/kafilaRoutes.js
@@ -2,7 +2,7 @@ const app = module.exports = require('express')();
 
 app.use('/', require('express').static('kafila'))
 
-app.use('/register', require('express').static('kafila/register.html'))
+//app.use('/register', require('express').static('kafila/register.html'))
 app.use('/contact', require('./contactRoute'))
 app.use('/thanx', require('express').static('kafila/thanx.html'))
 
